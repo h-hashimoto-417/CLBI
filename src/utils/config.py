d348a71f6eeae195b743c7f03587334eee1caa0d
h-hashimoto-417 <hitomi.seyomo484@outlook.jp>
2025-12-15 12:23:49 +0900

    予備実験開始

--- src/utils/config.py
+++ src/utils/config.py
@@ -57,13 +57,13 @@ USE_CACHE = False
 
 # ]
 
-PROJECT_RELEASE_LIST = [
+PROJECT_RELEASE_LIST_False = [
     'square.retrofit-1.0.0',
     'square.okhttp-1.0.0',
     'libgdx.libgdx-1.0.0',
 ]
 # バグのないもの、ファイルのデータがないものを削除した後の最終リスト
-PROJECT_RELEASE_LIST_False = [
+PROJECT_RELEASE_LIST = [
     'spring-projects.spring-boot-1.0.0', 'spring-projects.spring-boot-2.0.0', 'spring-projects.spring-boot-3.0.0',
     'iluwatar.java-design-patterns-1.0.0',
     'square.retrofit-1.0.0',
