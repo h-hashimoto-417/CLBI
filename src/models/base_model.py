33a29122d8001445c0ab8c3e1fb3db08415de081
h-hashimoto-417 <hitomi.seyomo484@outlook.jp>
2026-01-19 11:26:47 +0900

    抜けていたbugTypeを追加した

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -280,17 +280,16 @@ class BaseModel(object):
             file.write(f'{self.test_release},{prec},{recall},{far},{ce},{d2h},{mcc},{ifa},{r_20},{ER},{RI},{ratio}\n')
             
         append_title = True if not os.path.exists(f'{self.line_level_result_path}fn_types.csv') else False
-        title = 'release,CHANGE_IDENTIFIER,CHANGE_MODIFIER,DIFFERENT_METHOD_SAME_ARGS,CHANGE_NUMERAL,OVERLOAD_METHOD_MORE_ARGS,CHANGE_OPERATOR,CHANGE_CALLER_IN_FUNCTION_CALL,OVERLOAD_METHOD_DELETED_ARGS,MORE_SPECIFIC_IF,CHANGE_UNARY_OPERATOR,SWAP_BOOLEAN_LITERAL,SWAP_ARGUMENTS,CHANGE_OPERAND,ADD_THROWS_EXCEPTION,DELETE_THROWS_EXCEPTION\n'
+        title = 'release,CHANGE_IDENTIFIER,CHANGE_MODIFIER,DIFFERENT_METHOD_SAME_ARGS,CHANGE_NUMERAL,OVERLOAD_METHOD_MORE_ARGS,CHANGE_OPERATOR,LESS_SPECIFIC_IF,CHANGE_CALLER_IN_FUNCTION_CALL,OVERLOAD_METHOD_DELETED_ARGS,MORE_SPECIFIC_IF,CHANGE_UNARY_OPERATOR,SWAP_BOOLEAN_LITERAL,SWAP_ARGUMENTS,CHANGE_OPERAND,ADD_THROWS_EXCEPTION,DELETE_THROWS_EXCEPTION\n'
         count = Counter(fn_bug_types)
         with open(f'{self.line_level_result_path}fn_types.csv', 'a') as file:
             file.write(title) if append_title else None
-            file.write(f'{self.test_release},{count["CHANGE_IDENTIFIER"]},{count["CHANGE_MODIFIER"]},{count["DIFFERENT_METHOD_SAME_ARGS"]},{count["CHANGE_NUMERAL"]},{count["OVERLOAD_METHOD_MORE_ARGS"]},{count["CHANGE_OPERATOR"]},{count["CHANGE_CALLER_IN_FUNCTION_CALL"]},{count["OVERLOAD_METHOD_DELETED_ARGS"]},{count["MORE_SPECIFIC_IF"]},{count["CHANGE_UNARY_OPERATOR"]},{count["SWAP_BOOLEAN_LITERAL"]},{count["SWAP_ARGUMENTS"]},{count["CHANGE_OPERAND"]},{count["ADD_THROWS_EXCEPTION"]},{count["DELETE_THROWS_EXCEPTION"]}\n')
-
+            file.write(f'{self.test_release},{count["CHANGE_IDENTIFIER"]},{count["CHANGE_MODIFIER"]},{count["DIFFERENT_METHOD_SAME_ARGS"]},{count["CHANGE_NUMERAL"]},{count["OVERLOAD_METHOD_MORE_ARGS"]},{count["CHANGE_OPERATOR"]},{count["LESS_SPECIFIC_IF"]},{count["CHANGE_CALLER_IN_FUNCTION_CALL"]},{count["OVERLOAD_METHOD_DELETED_ARGS"]},{count["MORE_SPECIFIC_IF"]},{count["CHANGE_UNARY_OPERATOR"]},{count["SWAP_BOOLEAN_LITERAL"]},{count["SWAP_ARGUMENTS"]},{count["CHANGE_OPERAND"]},{count["ADD_THROWS_EXCEPTION"]},{count["DELETE_THROWS_EXCEPTION"]}\n')
         append_title = True if not os.path.exists(f'{self.line_level_result_path}tp_types.csv') else False
         count = Counter(tp_bug_types)
         with open(f'{self.line_level_result_path}tp_types.csv', 'a') as file:
             file.write(title) if append_title else None
-            file.write(f'{self.test_release},{count["CHANGE_IDENTIFIER"]},{count["CHANGE_MODIFIER"]},{count["DIFFERENT_METHOD_SAME_ARGS"]},{count["CHANGE_NUMERAL"]},{count["OVERLOAD_METHOD_MORE_ARGS"]},{count["CHANGE_OPERATOR"]},{count["CHANGE_CALLER_IN_FUNCTION_CALL"]},{count["OVERLOAD_METHOD_DELETED_ARGS"]},{count["MORE_SPECIFIC_IF"]},{count["CHANGE_UNARY_OPERATOR"]},{count["SWAP_BOOLEAN_LITERAL"]},{count["SWAP_ARGUMENTS"]},{count["CHANGE_OPERAND"]},{count["ADD_THROWS_EXCEPTION"]},{count["DELETE_THROWS_EXCEPTION"]}\n')
+            file.write(f'{self.test_release},{count["CHANGE_IDENTIFIER"]},{count["CHANGE_MODIFIER"]},{count["DIFFERENT_METHOD_SAME_ARGS"]},{count["CHANGE_NUMERAL"]},{count["OVERLOAD_METHOD_MORE_ARGS"]},{count["CHANGE_OPERATOR"]},{count["LESS_SPECIFIC_IF"]},{count["CHANGE_CALLER_IN_FUNCTION_CALL"]},{count["OVERLOAD_METHOD_DELETED_ARGS"]},{count["MORE_SPECIFIC_IF"]},{count["CHANGE_UNARY_OPERATOR"]},{count["SWAP_BOOLEAN_LITERAL"]},{count["SWAP_ARGUMENTS"]},{count["CHANGE_OPERAND"]},{count["ADD_THROWS_EXCEPTION"]},{count["DELETE_THROWS_EXCEPTION"]}\n')
         
         append_title = True if not os.path.exists(f'{self.line_level_result_path}TF_table.csv') else False
         title = 'release,TP,FP,FN,TN\n'
