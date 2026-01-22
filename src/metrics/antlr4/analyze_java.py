da2456d95d80b8587a183e433f24535ffde164f9
h-hashimoto-417 <hitomi.seyomo484@outlook.jp>
2026-01-22 18:00:03 +0900

    ANTLR環境構築

---
+++ src/metrics/antlr4/analyze_java.py
@@ -0,0 +1,5 @@
+from antlr4 import *
+from generated.JavaLexer import JavaLexer
+from generated.JavaParser import JavaParser
+from generated.JavaParserVisitor import JavaParserVisitor
+from generated.JavaParserListener import JavaParserListener
