d4b9aea9df49018973cfa31f462dbb55c7b45042
h-hashimoto-417 <hitomi.seyomo484@outlook.jp>
2026-01-30 10:41:02 +0900

    メトリクスを追加して実験してみる

--- src/models/glance.py
+++ src/models/glance.py
@@ -123,8 +123,9 @@ class Glance(BaseModel):
                 if nt == 0:
                     hit_count[line_index] = 0
                 else:
-                    hit_count[line_index] = nt * (nfc + 1)  # nt * (nfc + 1)では？
+                    #hit_count[line_index] = nt * (nfc + 1)  # nt * (nfc + 1)では？
                     #hit_count[line_index] = nt * nfc + 1  # 元のコードに戻す
+                    hit_count[line_index] = (nt + literal_count + operator_count) * (nfc + 1)  # リテラル数、演算子数も加味する
 
                 if 'for' in tokens_in_line:
                     cc_count[line_index] = True
