f379c485035fc779aad4c19091ea24abb158919f
h-hashimoto-417 <hitomi.seyomo484@outlook.jp>
2026-02-02 16:37:16 +0900

    初めの条件で実験

--- src/models/glance.py
+++ src/models/glance.py
@@ -123,11 +123,11 @@ class Glance(BaseModel):
                 if nt == 0:
                     hit_count[line_index] = 0
                 else:
-                    #hit_count[line_index] = nt * (nfc + 1)  # nt * (nfc + 1)では？
+                    hit_count[line_index] = nt * (nfc + 1)  # nt * (nfc + 1)では？
                     #hit_count[line_index] = nt * nfc + 1  # 元のコードに戻す
                     #hit_count[line_index] = (nt + literal_count + operator_count) * (nfc + 1)  # リテラル数、演算子数も加味する
                     #hit_count[line_index] = (1 + literal_count) * (1 + operator_count) * (nfc + 1)  # リテラル数、演算子数も乗算で加味する
-                    hit_count[line_index] = (literal_count + operator_count + 1) * (nfc + 1)  # リテラル数、演算子数も乗算で加味する
+                    #hit_count[line_index] = (literal_count + operator_count + 1) * (nfc + 1)  # リテラル数、演算子数も乗算で加味する
 
                 if 'for' in tokens_in_line:
                     cc_count[line_index] = True
